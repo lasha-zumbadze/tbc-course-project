f9bf3826acfd6b1241be8bf509a6c7abb34c4501
lasha-zumbadze <lashazumbadze6@gmail.com>
2024-03-09 09:57:20 -0700

    adding images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <nav className="navigation">
+          <div className="logo">
+            <p>The Venue</p>
+            <p>RESTAURANT</p>
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Menu</li>
+            <li>Delivery</li>
+            <li>Services</li>
+            <li>Contant</li>
+          </ul>
+          <div className="reservation">
+            <span>Reservation: </span>+995 123 456 789
+          </div>
+        </nav>
+        <div className="title">
+          <h1>The Venue is</h1>
+          <p>An Extraordinery Experience</p>
+        </div>
       </header>
-    </div>
+
+      <main>
+        <section className="about-us">
+          <div className="images">
+            <img src="images\about-1.jpg.webp" alt="cooking" />
+            <img src="images/about-2.jpg" alt="cooking" />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
-
-export default App;
